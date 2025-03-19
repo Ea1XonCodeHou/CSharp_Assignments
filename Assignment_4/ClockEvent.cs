05af2795f37bd736702373fea2037665db0faca3
Ea1XonCodeHou <975292748@qq.com>
2025-03-19 09:36:13 +0800

    Week04-Homework

---
+++ Assignment_4/ClockEvent.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading;
+
+public class AlarmClock
+{
+    public event Action Tick;
+    public event Action Alarm;
+
+    private int _seconds;
+    private int _alarmTime;
+
+    public AlarmClock(int alarmTime)
+    {
+        _alarmTime = alarmTime;
+        _seconds = 0;
+    }
+
+    public void Start()
+    {
+        while (_seconds <= _alarmTime)
+        {
+            Thread.Sleep(1000); // 模拟1秒
+            _seconds++;
+
+            Tick?.Invoke(); // 触发Tick事件
+
+            if (_seconds == _alarmTime)
+            {
+                Alarm?.Invoke(); // 触发Alarm事件
+            }
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        AlarmClock clock = new AlarmClock(5); // 5秒后闹钟响铃
+
+        clock.Tick += () => Console.WriteLine("嘀嗒...");
+        clock.Alarm += () => Console.WriteLine("闹钟响了！");
+
+        clock.Start();
+    }
+}
---
+++ Assignment_4/LinkList.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Net;
+namespace Week04CSharpLearning
+{
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+
+        public Node(T t)
+        {
+            this.Next = null;
+            this.Data = t;
+        }
+    }
+
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList()
+        {
+            tail = head = null;
+        }
+
+        public Node<T> Head
+        {
+            get=> head;
+        }
+
+        public void Add(T t)
+        {
+            Node<T> n=new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next= n;
+                tail = n;
+            }
+        }
+
+        public void forEach()
+        {
+            Node<T> n=head;
+            while (n != null)
+            {
+                Console.Write(n.Data + " ");
+                n=n.Next;
+            }
+            //Console.WriteLine(tail.Data);
+        }
+    }
+    class program
+    {
+        static void Main()
+        {
+            GenericList<String> list = new GenericList<String>();
+            list.Add("Hello");
+            list.Add("C#");
+            list.Add("Generic");
+            list.Add("LinkList");
+
+            list.forEach();
+        }
+    }
+}
